4abffdc9f8095c122c10e0973b3dd55f426beba0
sk7788775 <sk7788775@gmail.com>
2021-12-17 16:06:55 +0530

    Add gitignore,updated Procfile,and Updated ports

--- app.js
+++ app.js
@@ -135,6 +135,12 @@ app.get("/:shortUrl",async(req,res) => {
     res.redirect(shortUrl.full);
 });
 
-app.listen(3000,function() {
-    console.log("Server started at port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+
+app.listen(port,function() {
+    console.log("Server started successfully");
 });
